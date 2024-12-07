a8be3207fd16937844ddbebcc7ceedc989b21bac
Iago Bermúdez <iagombermudez@gmail.com>
2024-12-06 20:57:52 -0700

    added exercise 1.37

---
+++ chapter_1/exercise_1_37.rkt
@@ -0,0 +1,24 @@
+(define (cont-frac n d k)
+  (cond ((= k 0) (/ (n k) (d k)))
+        (else (/ ( n k)
+                 (+ (d k)
+                    (cont-frac n d (- k 1)))))))
+
+(cont-frac (lambda (i) 1.0)
+           (lambda (i) 1.0)
+           10)
+; K must equal 10 to get an approximation that is accurate to 4 decimal places
+
+
+; The trick to solve this is to start from the bottom up, if not is impossible
+; (or at least very difficult)
+(define (cont-frac-iter n d k)
+  (define (iter k i result)
+    (cond ((= i 0) result)
+          ((= i k) (iter (- i 1) (result + (/ (n i) (d i)))))
+          (else (iter (- i 1) (+ (d (- i 1) (/ (n i) result)))))))
+  (iter k 0))
+
+(cont-frac (lambda (i) 1.0)
+           (lambda (i) 1.0)
+           10)              
