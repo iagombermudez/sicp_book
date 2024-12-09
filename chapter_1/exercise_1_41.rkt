7c7f46e802d00d048f0fddc45427fa4e28e9e2ac
Iago Bermúdez <iagombermudez@gmail.com>
2024-12-09 15:12:04 -0700

    added exercise 1.41

---
+++ chapter_1/exercise_1_41.rkt
@@ -0,0 +1,11 @@
+(define (double f)
+  (lambda (x) (f (f x))))
+
+
+(((double (double double)) inc) 5)
+(((double (lambda (x) (double (double x)))) inc) 5)
+(((lambda (x) ((lambda (x) (double (double x)))
+               ((lambda (x) (double (double x))) x)))
+  inc) 5)
+((double (double (double (double inc )))) 5)
+; The result is 21
