cf7727b6fda2c9c7ec92a4f5e6cbc9d95708b252
Iago Bermúdez <iagombermudez@gmail.com>
2024-12-15 11:50:35 -0700

    add exercise 2.23

---
+++ chapter_2/exercise_2_23.rkt
@@ -0,0 +1,15 @@
+(for-each (lambda (x)
+            (newline)
+            (display x))
+          (list 57 321 88))
+
+(define (for-each proc items)
+  (if (null? items)
+      (newline)
+      (and (proc (car items))
+      (for-each proc (cdr items)))))
+
+(for-each (lambda (x)
+            (newline)
+            (display x))
+          (list 57 321 88))
