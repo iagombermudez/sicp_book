c8f12397ee7c0b42f08fb6ee7eeacbc2767dc2ea
Iago Bermúdez <iagombermudez@gmail.com>
2024-12-17 11:58:53 -0700

    add exercise 2.38

---
+++ chapter_2/exercise_2_38.rkt
@@ -0,0 +1,28 @@
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(fold-right / 1 (list 1 2 3))
+(/ 1 (/ 2 (/ 3 1)))
+(newline)
+(fold-left / 1 (list 1 2 3))
+(/ 0.5 3)
+(newline)
+(display (fold-right list nil (list 1 2 3)))
+(newline)
+(display (list 1 (list 2 (list 3 nil))))
+(newline)
+(newline)
+(display (fold-left list nil (list 1 2 3)))
+(newline)
+(display (list (list (list nil 1) 2) 3))
