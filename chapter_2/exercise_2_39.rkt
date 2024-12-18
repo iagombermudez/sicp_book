b35b11742371f29de4ca21f57b58dc810e711a5c
Iago Bermúdez <iagombermudez@gmail.com>
2024-12-18 12:52:37 -0700

    add exercise 2.39

---
+++ chapter_2/exercise_2_39.rkt
@@ -0,0 +1,24 @@
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (reverse sequence)
+  (fold-right (lambda (x y) (append y (list x))) nil sequence))
+
+(display (reverse (list 1 2 3)))
+(newline)
+
+(define (reverse sequence)
+   (fold-left (lambda (x y) (cons y x)) nil sequence))
+
+(display (reverse (list 1 2 3)))
