ad1df16ba45333d254b2a3a0815beaa6e88e5e1b
Iago Bermúdez <iagombermudez@gmail.com>
2024-12-20 10:25:20 -0700

    added exercise 2.43

---
+++ chapter_2/exercise_2_43.rkt
@@ -0,0 +1,37 @@
+; Original (Time T)
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position
+                    new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (new-row)
+            (map (lambda (rest-of-queens)
+                   (adjoin-position new-row k rest-of-queens))
+                 (queen-cols (- k 1))))
+          (enumerate-interval 1 board-size))
+          )))
+  (queen-cols board-size))
+
+
+; The difference is that for every single column, it executes queens-cols
+; board-size times. This happens for every iteration of queens-col, meaning
+; that it will execute it 7 times, then 6 times, then 5 times....
+; This means that that it will take ((n-1)!T board-size
+; (I wouldn't trust this numbers too much)
