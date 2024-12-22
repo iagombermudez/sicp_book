f5e16b05241166177b349965622c290f6cd2daa8
Iago Bermúdez <iagombermudez@gmail.com>
2024-12-21 22:59:27 -0700

    added exercise 2.48

---
+++ chapter_2/exercise_2_48.rkt
@@ -0,0 +1,13 @@
+(define (make-vect xcor ycor)
+  (cons xcor ycor))
+(define (xcor-vect vect)
+  (car vect))
+(define (ycor-vect vect)
+  (cdr vect))
+
+(define (make-segment start end)
+  (cons start end))
+(define (start-segment segment)
+  (car segment))
+(define (end-segment segment)
+  (cdr segment))
