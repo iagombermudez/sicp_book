d1f55fd53c78b79d78f89cfd1a5c751dcb9a4088
Iago Bermúdez <iagombermudez@gmail.com>
2025-01-03 01:11:50 -0700

    added exercise 2.69

--- chapter_2/exercise_2_68.rkt
+++ chapter_2/exercise_2_68.rkt
@@ -58,7 +58,8 @@
                    (make-leaf 'B 2)
                    (make-code-tree (make-leaf 'D 1)
                                    (make-leaf 'C 1)))))
-
+(display sample-tree)
+(newline)
 (define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
 
 
---
+++ chapter_2/exercise_2_69.rkt
@@ -0,0 +1,61 @@
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair) 
+                               (cadr pair))
+                    (make-leaf-set (cdr pairs))))))
+(define (left-branch tree) (car tree))
+(define (right-branch tree) (cadr tree))
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (generate-huffman-tree pairs)
+  (successive-merge (make-leaf-set pairs)))
+
+(define (successive-merge leaf-set)
+  (display leaf-set)
+  (newline)
+  (if (null? (cdr leaf-set)) (car leaf-set)
+      (successive-merge (adjoin-set
+                         (make-code-tree (car leaf-set) (cadr leaf-set))
+                        (cddr leaf-set)))))
+
+
+(define (add-new-merge leaf leaf-set)
+  (cond ((null? leaf-set) (list leaf))
+        ((> (weight-leaf leaf) (weight-leaf (car leaf-set)))
+         (cons (car leaf-set) (add-new-merge leaf (cdr leaf-set))))
+        (else (cons leaf leaf-set))))
+         
+
+(define pairs (list (list 'A 8) (list 'B 3)
+                    (list 'C 1) (list 'D 1)
+                    (list 'E 1) (list 'F 1)
+                    (list 'G 1) (list 'H 1)))
+
+(newline)
+(display (generate-huffman-tree pairs))
